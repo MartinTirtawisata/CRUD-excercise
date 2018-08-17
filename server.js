5ec36352010d92d668226e963f710887a0ef1330
martintirtawisata <martin.tirtawisata@hotmail.com>
2018-08-17 15:35:13 -0700

    adding chai integration

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(express.json());
+
 const blogpostRouter = require('./blogpostRouter');
 
-app.use('/blogposts', blogpostRouter);
-app.use(express.json())
+//log the HTTP layer
+app.use(morgan("common"));
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`Your app is listnening on port ${process.env.PORT || 8080}`)
-})
+//requests comes to /blogposts --> we route it to blogpostRouter
+app.use('/blogposts', blogpostRouter);
+
+//since both runServer and closeServer needs the same 'server' object; it is run outside
+let server;
+
+function runServer() {
+    const myPort = process.env.PORT || 8080;
+    return new Promise ((resolve, reject), function() {
+        server = app.listen(myPort, function() {
+            console.log(`Your app is listening to port ${myPort} !!!`)
+            resolve(server);
+        })
+        .on("error", err, function(){
+            reject(err);
+        });
+    });
+}
+
+//?? Confused at this part ??
+function closeServer (){
+    return new Promise ((resolve, reject), function(){
+        console.log("closing the server woohoo");
+        server.close(err, function(){
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+//??what is require.main and module??
+if (require.amin === module){
+    runServer().catch(err, function(){ console.error(err)})
+}
+
+module.exports = {app, runServer, closeServer};
---
+++ test/test-blogpost.js
@@ -0,0 +1,23 @@
+const chai_test = require("chai");
+const chai_http = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const i_expect = chai.expect;
+
+chai.use(chai_http);
+
+describe("Blog-post content", function(){
+    before(function(){
+        //before = runs this code before anything else in this block
+        return runServer();
+    });
+
+    after(function(){
+        //after = run this block after anything else in this block
+        return closeServer();
+    });
+
+
+    testings
+});
